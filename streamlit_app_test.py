c20c43feedbe893fa3b0f102a3c708bc8b331951
jdessus <79161065+jdessus@users.noreply.github.com>
2022-08-24 12:24:55 +0200

    Add files via upload

--- streamlit_app_test.py
+++ streamlit_app_test.py
@@ -9,6 +9,18 @@ st.write("Data:")
 
 link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
 df_cars= pd.read_csv(link)
+
+# Create a list of possible values and multiselect menu with them in it.
+continents = df_cars['continent'].unique()
+continents_selected = st.multiselect('Select continents', continents)
+
+# Mask to filter dataframe
+mask_continents = df_cars['continent'].isin(continents_selected)
+
+df_cars = df_cars[mask_continents]
+
+
+
 st.write(df_cars)
 
 
